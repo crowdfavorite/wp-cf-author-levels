d0ddf5447d78b4e062f0628201fd31b8bb828806
sklein <sklein@546ca710-db30-0410-8887-d9dd8e66aff0>
2009-05-28 19:59:48 +0000

    adding 1.1 tag
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-author-levels/tags/1.1@11464 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-author-levels.php
+++ cf-author-levels.php
@@ -3,7 +3,7 @@
 Plugin Name: CF Author Levels
 Plugin URI: http://crowdfavorite.com
 Description: Advanced options for author levels
-Version: 1.1b1
+Version: 1.1
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */
