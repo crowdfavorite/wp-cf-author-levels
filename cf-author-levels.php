04b3c0fbb03953cbc0203bc3a4666be97a857ac5
Steven Mathias <steve@crowdfavorite.com>
2012-06-07 14:52:05 -0600

    Updated changelog and version number

--- cf-author-levels.php
+++ cf-author-levels.php
@@ -3,7 +3,7 @@
 Plugin Name: CF Author Levels
 Plugin URI: http://crowdfavorite.com
 Description: Advanced options for author levels
-Version: 1.4
+Version: 1.4.1
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */
